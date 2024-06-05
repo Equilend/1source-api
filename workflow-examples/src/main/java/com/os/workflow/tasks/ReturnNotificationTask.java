314f1df4bf7fdf938c80611619c09e4158344eb9
mschoenberg <matthew.schoenberg@equilend.com>
2024-06-05 10:18:20 -0400

    Added return notification example

---
+++ workflow-examples/src/main/java/com/os/workflow/tasks/ReturnNotificationTask.java
@@ -0,0 +1,76 @@
+package com.os.workflow.tasks;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.os.workflow.AuthConfig;
+import com.os.workflow.AuthToken;
+import io.swagger.v1_0_5_20240428.client.model.LedgerResponse;
+import io.swagger.v1_0_5_20240428.client.model.LocalDateTypeAdapter;
+import io.swagger.v1_0_5_20240428.client.model.OffsetDateTimeTypeAdapter;
+import io.swagger.v1_0_5_20240428.client.model.ReturnProposal;
+import io.swagger.v1_0_5_20240428.client.model.SettlementType;
+import reactor.core.publisher.Mono;
+
+public class ReturnNotificationTask implements Tasklet, StepExecutionListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(ReturnNotificationTask.class);
+
+	private AuthToken ledgerToken;
+
+	@Autowired
+	WebClient restWebClient;
+
+	@Autowired
+	AuthConfig authConfig;
+
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		ledgerToken = (AuthToken) stepExecution.getJobExecution().getExecutionContext().get("ledgerToken");
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+		Random random = new Random();
+		
+		ReturnProposal proposal = new ReturnProposal();
+		
+		proposal.setQuantity(((((random.nextInt(1000 - 100) + 1000))+99)/100)*100);
+		proposal.setReturnDate(LocalDate.now());
+		proposal.setReturnSettlementDate(LocalDate.now());
+		proposal.setSettlementType(SettlementType.DVP);
+		proposal.setCollateralValue(100000.00);
+
+		Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+				.registerTypeAdapter(OffsetDateTime.class, new OffsetDateTimeTypeAdapter()).create();
+
+		logger.debug(gson.toJson(proposal));
+
+		LedgerResponse ledgerResponse = restWebClient.post().uri("/contracts/" + authConfig.getContract_id() + "/returns").contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(proposal).headers(h -> h.setBearerAuth(ledgerToken.getAccess_token())).retrieve()
+				.onStatus(HttpStatusCode::is4xxClientError, response -> {
+					return Mono.empty();
+				}).bodyToMono(LedgerResponse.class).block();
+
+		logger.debug("Ledger Response: " + ledgerResponse);
+
+		return RepeatStatus.FINISHED;
+	}
+
+}
