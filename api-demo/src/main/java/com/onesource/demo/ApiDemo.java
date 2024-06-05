6bdf9e89403407573a4f8260861f9cfe89c69576
gsmith <guian.smith@equilend.com>
2024-06-05 17:20:32 -0400

    GNS: Implemented additional API methods

--- api-demo/src/main/java/com/onesource/demo/ApiDemo.java
+++ api-demo/src/main/java/com/onesource/demo/ApiDemo.java
@@ -2,8 +2,8 @@ package com.onesource.demo;
 
 import com.onesource.demo.util.ApiHelper;
 import com.onesource.demo.util.JsonGenerator;
-import org.apache.commons.lang3.StringUtils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -19,19 +19,22 @@ public class ApiDemo {
         String borrowerToken = apiHelper.getAuthToken("TestBorrower1User", "FqnNQyUwaenQ8K3h");
         String lenderToken = apiHelper.getAuthToken("TestLender1User", "fjmxVeKzpzUDg3YJ");
 
-        String sinceTime = "2024-06-01T01:00:00.000Z";
+        String sinceTime = "2024-06-05T01:00:00.000Z";
 //        apiHelper.getEntity(lenderToken, "parties");
 //        apiHelper.getEntity(token, "agreements");
 //        apiHelper.getEntity(token, "delegations"); // Does not work... not implemented yet
         apiHelper.getEntity(lenderToken, "events", "since=" + sinceTime);
         apiHelper.getEntity(lenderToken, "contracts", "since=" + sinceTime);
+//        apiHelper.getEntity(lenderToken, "returns", "since=" + sinceTime);
+//        apiHelper.getEntity(lenderToken, "recalls", "since=" + sinceTime);
 
 //        approveContract(borrowerToken, "cf796f2f-9d68-4efe-b5d4-887223f48d9a");
-//        submitNewReturn(borrowerToken, "cf796f2f-9d68-4efe-b5d4-887223f48d9a";
+//        submitNewReturn(borrowerToken, "a5ee63ed-2d63-4505-9478-97ab41d05b7e");
 
         submitNewContract(lenderToken);
 
-        // From contract returned above, find first one in PROPOSED state, should be the one we just submitted
+        // Get new events and look for the latest contract in PROPOSED state,
+        // it's probably be the one we just submitted... so we'll try to approve it and then submit a return
         String contractId = getLatestContractId(lenderToken);
 
         if (StringUtils.isEmpty(contractId)) {
@@ -41,30 +44,42 @@ public class ApiDemo {
             System.out.println("Found contractId: " + contractId);
 
             approveContract(borrowerToken, contractId);
+            patchContractSettled(borrowerToken, contractId);
+            patchContractSettled(lenderToken, contractId);
             submitNewReturn(borrowerToken, contractId);
         }
 
         apiHelper.getEntity(lenderToken, "contracts", "since=" + sinceTime);
+        apiHelper.getEntity(lenderToken, "returns", "since=" + sinceTime);
         System.out.println("Done");
     }
 
     private static void submitNewContract(String lenderToken) {
-        JSONObject contractProposalJson = JsonGenerator.generateContractProposalJson();
-        System.out.println("\nSubmit contract:\n" + contractProposalJson.toString(2));
-        apiHelper.postContractProposal(lenderToken, contractProposalJson.toString());
+        JSONObject body = JsonGenerator.generateContractProposalJson();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("\nSubmit contract:\n" + body.toString(2));
+        apiHelper.postContractProposal(lenderToken, body.toString());
     }
 
     private static void approveContract(String borrowerToken, String contractId) {
-        JSONObject contractApproveJson = JsonGenerator.generateContractApprovalJson(contractId);
+        JSONObject body = JsonGenerator.generateContractApprovalJson(contractId);
         System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("Approve contract:\n" + contractApproveJson.toString(2));
-        apiHelper.postContractAction(borrowerToken, contractApproveJson.toString(), contractId, "approve");
+        System.out.println("Approve contract:\n" + body.toString(2));
+        apiHelper.postContractAction(borrowerToken, body.toString(), contractId, "approve");
+    }
+
+    private static void patchContractSettled(String token, String contractId) {
+        JSONObject body = JsonGenerator.generateSettlementStatusUpdateJson("SETTLED");
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Update contract status:\n" + body.toString(2));
+        apiHelper.patchContract(token, body.toString(), contractId);
     }
 
     private static void submitNewReturn(String borrowerToken, String contractId) {
-        JSONObject returnProposalJson = JsonGenerator.generateReturnProposalJson(contractId);
-        System.out.println("Submit return:\n" + returnProposalJson.toString(2));
-        apiHelper.postReturnRecallProposal(borrowerToken, returnProposalJson.toString(), contractId, "returns");
+        JSONObject body = JsonGenerator.generateReturnProposalJson(contractId);
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Submit return:\n" + body.toString(2));
+        apiHelper.postReturnRecallProposal(borrowerToken, body.toString(), contractId, "returns");
     }
 
     /**
