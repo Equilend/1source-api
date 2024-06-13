9c14e0e8c5ac68dc40515367ddaf0128875d2a2e
mschoenberg <matthew.schoenberg@equilend.com>
2024-06-13 11:01:17 -0400

    added return cancel

---
+++ workflow-examples/src/main/java/com/os/workflow/tasks/ReturnCancelNotificationTask.java
@@ -0,0 +1,55 @@
+package com.os.workflow.tasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.os.workflow.AuthToken;
+import com.os.workflow.WorkflowConfig;
+import com.os.client.model.LedgerResponse;
+import reactor.core.publisher.Mono;
+
+public class ReturnCancelNotificationTask implements Tasklet, StepExecutionListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(ReturnCancelNotificationTask.class);
+
+	private AuthToken ledgerToken;
+	//private Return returnObj;
+
+	@Autowired
+	WebClient restWebClient;
+
+	@Autowired
+	WorkflowConfig workflowConfig;
+
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		ledgerToken = (AuthToken) stepExecution.getJobExecution().getExecutionContext().get("ledgerToken");
+		//returnObj = (Return) stepExecution.getJobExecution().getExecutionContext().get("returnObj");
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+		LedgerResponse ledgerResponse = restWebClient.post().uri("/contracts/" + workflowConfig.getContract_id() + "/returns/" + workflowConfig.getReturn_id() + "/cancel").contentType(MediaType.APPLICATION_JSON)
+				//.bodyValue(json)
+				.headers(h -> h.setBearerAuth(ledgerToken.getAccess_token())).retrieve()
+				.onStatus(HttpStatusCode::is4xxClientError, response -> {
+					return Mono.empty();
+				}).bodyToMono(LedgerResponse.class).block();
+
+		logger.debug("Ledger Response: " + ledgerResponse);
+
+		return RepeatStatus.FINISHED;
+	}
+
+}
