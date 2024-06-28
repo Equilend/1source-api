c3401354830617cf3a430dbd0856714ba7e0c186
mschoenberg <matthew.schoenberg@equilend.com>
2024-06-28 10:04:00 -0400

    Added recall initiate

---
+++ workflow-examples/src/main/java/com/os/workflow/tasks/RecallInitiationTask.java
@@ -0,0 +1,81 @@
+package com.os.workflow.tasks;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.os.client.model.LedgerResponse;
+import com.os.client.model.RecallProposal;
+import com.os.workflow.AuthToken;
+import com.os.workflow.LocalDateTypeGsonAdapter;
+import com.os.workflow.OffsetDateTimeTypeGsonAdapter;
+import com.os.workflow.WorkflowConfig;
+
+import reactor.core.publisher.Mono;
+
+public class RecallInitiationTask implements Tasklet, StepExecutionListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(RecallInitiationTask.class);
+
+	private AuthToken ledgerToken;
+
+	@Autowired
+	WebClient restWebClient;
+
+	@Autowired
+	WorkflowConfig workflowConfig;
+
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		ledgerToken = (AuthToken) stepExecution.getJobExecution().getExecutionContext().get("ledgerToken");
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+		Random random = new Random();
+		
+		RecallProposal proposal = new RecallProposal();
+		
+		LocalDate currentDate = LocalDate.now(ZoneId.of("UTC"));
+		
+		proposal.setQuantity(((((random.nextInt(1000 - 100) + 1000))+99)/100)*100);
+		proposal.setRecallDate(currentDate);
+		proposal.setRecallDueDate(currentDate.plusDays(3));
+
+		Gson gson = new GsonBuilder()
+			    .registerTypeAdapter(LocalDate.class, new LocalDateTypeGsonAdapter())
+			    .registerTypeAdapter(OffsetDateTime.class, new OffsetDateTimeTypeGsonAdapter())
+			    .create();
+
+		String json = gson.toJson(proposal);
+		logger.debug(json);
+
+		LedgerResponse ledgerResponse = restWebClient.post().uri("/contracts/" + workflowConfig.getContract_id() + "/recalls").contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(json).headers(h -> h.setBearerAuth(ledgerToken.getAccess_token())).retrieve()
+				.onStatus(HttpStatusCode::is4xxClientError, response -> {
+					return Mono.empty();
+				}).bodyToMono(LedgerResponse.class).block();
+
+		logger.debug("Ledger Response: " + ledgerResponse);
+
+		return RepeatStatus.FINISHED;
+	}
+
+}
