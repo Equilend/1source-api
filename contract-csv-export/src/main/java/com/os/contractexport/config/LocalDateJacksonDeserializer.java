d9cfd010b697327ef5fcf59579e3dbec073c30a6
mschoenberg <matthew.schoenberg@equilend.com>
2024-08-27 10:03:57 -0400

    updated to 1.1.0 spec

---
+++ contract-csv-export/src/main/java/com/os/contractexport/config/LocalDateJacksonDeserializer.java
@@ -0,0 +1,27 @@
+package com.os.contractexport.config;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class LocalDateJacksonDeserializer extends JsonDeserializer<LocalDate> {
+	
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE;
+
+	@Override
+	public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+			throws IOException {
+		String dateAsString = jsonParser.getText();
+		if (dateAsString == null) {
+			throw new IOException("dateAsString argument is null.");
+		}
+		
+		LocalDate date = LocalDate.parse(dateAsString, DATE_TIME_FORMATTER);
+		
+		return date;
+	}
+}
