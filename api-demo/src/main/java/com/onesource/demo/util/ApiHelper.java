e7428af3739c0aff1832a0903f28b839c28f1257
gsmith <guian.smith@equilend.com>
2024-08-28 17:22:46 -0400

    GNS: Upgraded api-demo to latest 1Source API, and added functions

--- api-demo/src/main/java/com/onesource/demo/util/ApiHelper.java
+++ api-demo/src/main/java/com/onesource/demo/util/ApiHelper.java
@@ -3,6 +3,7 @@ package com.onesource.demo.util;
 import com.intellecteu.onesource.integration.services.client.onesource.invoker.auth.Authentication;
 import com.intellecteu.onesource.integration.services.client.onesource.invoker.auth.OAuth;
 import com.intellecteu.onesource.integration.services.client.onesource.invoker.auth.RFC3339DateFormat;
+import com.onesource.demo.ApiDemoApp;
 import com.onesource.demo.dto.LedgerResponseDTO;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
@@ -37,6 +38,7 @@ public class ApiHelper {
     private Map<String, Authentication> authentications;
     private DateFormat dateFormat;
     private HttpHeaders defaultHeaders = new HttpHeaders();
+    private String refreshToken = null;
 
     public ApiHelper() {
         restTemplate = new RestTemplate();
@@ -62,6 +64,8 @@ public class ApiHelper {
     }
 
     public String getAuthToken(String userName, String password) {
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Acquire Auth token for " + userName);
         try {
             Map<String, String> formData = new HashMap<>();
             formData.put("auth_type", "BEARER"); // Cloned these from Python app
@@ -78,11 +82,66 @@ public class ApiHelper {
                     .POST(HttpRequest.BodyPublishers.ofString(getFormDataAsString(formData)))
                     .build();
 
-            System.out.println(request.method() + " " + request.uri() + "\nHeaders: " + request.headers() + "\n");
+            System.out.println(request.method() + " " + request.uri() );
+//            System.out.println(request.method() + " " + request.uri() + "\nRequest Headers: ");
+//            request.headers().map().forEach((k, v) -> System.out.println("    " + k + ": " + v));
+
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            if (response.statusCode() != 200) {
+                System.out.println("Failed to get token for " + userName + ": " + response.statusCode() + " " + response.body());
+                return null;
+            }
+
+//            System.out.println("Response Headers: ");
+//            response.headers().map().forEach((k, v) -> System.out.println("    " + k + ": " + v));
+
             JSONObject obj = new JSONObject(response.body());
-            return obj.getString("access_token");
+//            System.out.println("Response for " + userName + ":\n" + obj.toString(4));
+
+            String token     = obj.getString("access_token");
+            Integer expiresIn = obj.getInt("expires_in");
+            System.out.println("Token acquired for " + userName + ", expires in " + expiresIn + " seconds");
+
+            if (refreshToken == null && obj.has("refresh_token")) {
+                refreshToken = obj.getString("refresh_token");
+            }
+            return token;
+        }
+        catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String refreshAuthToken() {
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Refresh token...");
+        try {
+            Map<String, String> formData = new HashMap<>();
+            formData.put("grant_type", "refresh_token");
+            formData.put("refresh_token", refreshToken);
+            formData.put("client_id", "canton-participant1-client"); // Use your actual client ID
+            formData.put("client_secret", "c0a05c2d-ac70-472a-ac4f-38b80dba28d8"); // Use your actual client secret
+
+            HttpRequest request = HttpRequest.newBuilder(new URI("https://stageauth.equilend.com/auth/realms/1Source/protocol/openid-connect/token"))
+                    .version(HttpClient.Version.HTTP_2)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString(getFormDataAsString(formData)))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) {
+                System.out.println("Failed to refresh token: " + response.statusCode() + " " + response.body());
+                return null;
+            }
+
+            JSONObject obj = new JSONObject(response.body());
+            System.out.println("Refresh rsponse:\n" + obj.toString(4) + "\n");
+
+            String newAccessToken = obj.getString("access_token");
+            System.out.println("New access token: " + newAccessToken);
+            return newAccessToken;
         }
         catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
@@ -90,10 +149,89 @@ public class ApiHelper {
     }
 
     public String getEntity(String token, String entity) {
-        return getEntity(token, entity, null);
+        return getEntity(token, entity, null, null);
     }
 
     public String getEntity(String token, String entity, MultiValueMap<String, String> queryParams) {
+        return getEntity(token, entity, null, queryParams);
+    }
+
+    public String getEntity(String token, String entity, String entityId) {
+        return getEntity(token, entity, entityId, null);
+    }
+
+    private String getEntity(String token, String entity, String entityId, MultiValueMap<String,String> queryParams) {
+        if (token == null) {
+            System.out.println("token is null, API is not initialized yet");
+            return null;
+        }
+
+        String submitter = ApiDemoApp.getSubmitter(token);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(basePath)
+                .queryParams(queryParams)
+                .path("/ledger/" + entity);
+        if(entityId != null) {
+            builder.path("/" + entityId);
+        }
+
+        RequestEntity.BodyBuilder requestBuilder = RequestEntity.method(HttpMethod.GET, builder.build().toUri());
+
+        HttpHeaders headerParams = new HttpHeaders();
+        headerParams.add("Authorization", "Bearer " + token);
+
+        String[] accepts = { "application/json" };
+        List<MediaType> accept = selectHeaderAccept(accepts);
+        requestBuilder.accept(accept.toArray(new MediaType[accept.size()]));
+
+        String[] contentTypes = { "application/json" };
+        MediaType contentType = MediaType.APPLICATION_JSON;
+        requestBuilder.contentType(contentType);
+
+        String[] authNames = new String[] { "stage_auth" };
+
+        updateParamsForAuth(authNames, queryParams, headerParams);
+
+        addHeadersToRequest(headerParams, requestBuilder);
+        addHeadersToRequest(defaultHeaders, requestBuilder);
+
+        String body = "";
+        ParameterizedTypeReference<String> returnType = new ParameterizedTypeReference<String>() {};
+        RequestEntity<Object> requestEntity = requestBuilder.body(selectBody(body, /*formParams*/ null, contentType));
+
+        System.out.println("GET " + StringUtils.capitalize(entity) + "  " + builder.toUriString() + "\n");
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, returnType);
+
+        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
+
+            if (responseEntity.getStatusCode().value() == 404) {  // Not found
+                System.out.println("API request GET " + entity + " failed 404 - NOT FOUND\n" + responseEntity);
+            }
+            else {
+                System.out.println("API request GET " + entity + " failed with status " + responseEntity.getStatusCode() + "\n" + responseEntity);
+            }
+
+            if (responseEntity.hasBody()) {
+                System.out.println("response: " + responseEntity.getBody());
+            }
+//            throw new HttpClientErrorException(responseEntity.getStatusCode(),
+//                    "API returned " + responseEntity.getStatusCode()
+//                            + " and it wasn't handled by the RestTemplate error handler");
+            return null;
+        }
+
+        String responseBody = responseEntity.getBody();
+//        System.out.println("response: " + responseBody);
+
+        String resultStr = ppJson(responseBody);
+
+        String msg = (submitter == null ? "" : " for " + submitter);
+        System.out.println(StringUtils.capitalize(entity) + msg + ":\n" + resultStr);
+        return responseBody;
+    }
+
+    private String getEntitySAVE(String token, String entity, MultiValueMap<String,String> queryParams, String tag) {
         System.out.println("------------------------------------------------------------------------------------");
 
         String path = basePath + "/ledger/" + entity;
@@ -154,7 +292,8 @@ public class ApiHelper {
 
         String resultStr = ppJson(responseBody);
 
-        System.out.println(StringUtils.capitalize(entity) + ":\n" + resultStr);
+        String msg = (tag == null ? "" : " for " + tag);
+        System.out.println(StringUtils.capitalize(entity) + msg + ":\n" + resultStr);
         return responseBody;
     }
 
@@ -166,13 +305,33 @@ public class ApiHelper {
         return submitApiPost(HttpMethod.POST, token, body, contractId, contractAction, null, null, null);  // Approve, Cancel, Decline a contract
     }
 
-    public ResponseEntity<LedgerResponseDTO> patchContract(String token, /*ContractsContractIdBodyDTO*/ String body, String contractId) {
+    public ResponseEntity<LedgerResponseDTO> postEntityAction(String token, String body, String contractId, String entity, String entityId, String entityAction) {
+        return submitApiPost(HttpMethod.POST, token, body, contractId, null, entity, entityId, entityAction);  // Approve, Cancel, Decline a rerate, return or recall
+    }
+
+    public ResponseEntity<LedgerResponseDTO> patchContract(String token, String body, String contractId) {
         return submitApiPost(HttpMethod.PATCH, token, body, contractId, null, null, null, null); // Patch a contract (SettlementStatusUpdate, etc)
+    }
+
+    public ResponseEntity<LedgerResponseDTO> patchReturn(String token, String body, String contractId, String returnId) {
+        return submitApiPost(HttpMethod.PATCH, token, body, contractId, null, "returns", returnId, null); // Patch a return (SettlementStatusUpdate, etc)
     }
 
     public ResponseEntity<LedgerResponseDTO> postReturnRecallProposal(String token, String body, String contractId, String entity) {
         return submitApiPost(HttpMethod.POST, token, body, contractId, null, entity, null, null);  // Post a new return or recall proposal
     }
+
+    public ResponseEntity<LedgerResponseDTO> postReturnAcknowledge(String token, String body, String contractId, String returnId, String ackType) {
+        return submitApiPost(HttpMethod.POST, token, body, contractId, null, "returns", returnId, "acknowledge");
+    }
+
+    public ResponseEntity<LedgerResponseDTO> postReturnCancel(String token, String contractId, String returnId) {
+        return submitApiPost(HttpMethod.POST, token, null, contractId, null, "returns", returnId, "cancel");
+    }
+
+    public ResponseEntity<LedgerResponseDTO> postRerate(String token, String body, String contractId, String entity) {
+            return submitApiPost(HttpMethod.POST, token, body, contractId, null, entity, null, null);
+        }
 
     /**
      * @param httpMethod     = POST or PATCH
@@ -187,11 +346,11 @@ public class ApiHelper {
      */
     private ResponseEntity<LedgerResponseDTO> submitApiPost(HttpMethod httpMethod, String token, String body, String contractId, String contractAction,
                                                             String entity, String entityId, String entityAction) {
-        if (body == null) {
+        if (body == null && !"cancel".equals(entityAction) && !"approve".equals(entityAction)) {
             throw new RuntimeException("Missing required parameter 'body'");
         }
 
-        String pathStr = "/ledger/contracts";
+        String pathStr = "/ledger/loans";
 
         Map<String, Object> uriVariables = new HashMap<String, Object>();
 
@@ -229,7 +388,6 @@ public class ApiHelper {
 
     private <T> ResponseEntity<T> getApiPostResponse(HttpMethod httpMethod, String token, String body,
                                                                  String path, ParameterizedTypeReference<T> returnType) {
-
         HttpHeaders headerParams = new HttpHeaders();
         headerParams.add("Authorization", "Bearer " + token);
 
@@ -362,6 +520,9 @@ public class ApiHelper {
         return formattedJson;
     }
 
+    /**
+     * convert a Map<String, String> to a URL-encoded form data string.
+     */
     private String getFormDataAsString(Map<String, String> formData) {
         StringBuilder formBodyBuilder = new StringBuilder();
         for (Map.Entry<String, String> singleEntry : formData.entrySet()) {
@@ -372,7 +533,7 @@ public class ApiHelper {
             formBodyBuilder.append("=");
             formBodyBuilder.append(URLEncoder.encode(singleEntry.getValue(), StandardCharsets.UTF_8));
         }
-        System.out.println("getFormDataAsString: " + formBodyBuilder.toString());
+//        System.out.println("getFormDataAsString: " + formBodyBuilder.toString());
         return formBodyBuilder.toString();
     }
 }
